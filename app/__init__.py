dd558b02122d6e2030149ac7bc8979d3d4d6db7d
Tamalee Pottinger <tammylee816@yahoo.com>
2020-02-16 02:26:04 -0500

    Completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from flask_mail import Mail
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sunf!ower'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = '2525' # (or try 2525)
+
+
+mail = Mail(app)
 from app import views
